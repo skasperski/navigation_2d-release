55fa65cae76919d2e1c97ee9e5ce9e02fe542950
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2017-03-28 09:44:08 +0200

    Rebase from 'upstream'

--- nav2d_karto/src/MapperNode.cpp
+++ nav2d_karto/src/MapperNode.cpp
@@ -2,12 +2,7 @@
 
 #include <nav2d_karto/MultiMapper.h>
 #include <nav2d_karto/SpaSolver.h>
-
-#ifdef USE_G2O
-#include <nav2d_karto/G2oSolver.h>
-#else
 #include <nav2d_karto/SpaSolver.h>
-#endif
 
 int main(int argc, char **argv)
 {
@@ -16,27 +11,21 @@ int main(int argc, char **argv)
 	ros::NodeHandle node;
 
 	// Create a scan-solver
-#ifdef USE_G2O
-	G2oSolver* solver = new G2oSolver();
-#else
 	SpaSolver* solver = new SpaSolver();
-#endif
 
 	// Create the MultiMapper
-	MultiMapper* mapper = new MultiMapper();
-	mapper->setScanSolver(solver);
+	MultiMapper mapper;
+	mapper.setScanSolver(solver);
 
 	// Start main loop
 	ros::Rate publishRate(10);
 	while(ros::ok())
 	{
-		mapper->publishTransform();
+		mapper.publishTransform();
 		ros::spinOnce();
 		publishRate.sleep();
 	}
 
 	// Quit
-	delete mapper;
-	delete solver;
 	return 0;
 }
